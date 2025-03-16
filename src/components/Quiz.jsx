14041babdba92e1e92f74bc9886463f38d236ac0
Suhas Bachhav <59196135+suhasbachhav@users.noreply.github.com>
2025-03-16 09:52:48 +0000

    quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,49 @@
 import { useState } from "react";
+import QUESTIONS from "../Questions";
+import quizCompletedLogo from "../assets/quiz-complete.png"
 
 function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex ] = useState(0);
     const [userAnswers, setUserAnswers ] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+    const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
+
+    function handleSelectAnswer(selectedAnswer){
+        setUserAnswers(prevUserAnswers =>{
+            return [...prevUserAnswers, selectedAnswer]
+        });
+    }
+
+    if(quizIsCompleted){
+        return (
+            <div id="summary">
+                <img src={quizCompletedLogo} alt="Trophy Icon" />
+                <h2>
+                    Quiz Completed!
+                </h2>
+            </div>
+        )
+    }
+
+    const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffleAnswers.map(()=> Math.random() - 0.5);
+
     return (
-        <p>Currently active questions</p>
+        <div id="quiz">
+            <div id="question">
+                <h2>
+                    {QUESTIONS[activeQuestionIndex].text}
+                </h2>
+                <ul id="answers">
+                    {shuffleAnswers.map(answer=>
+                        <li key={answer} className="answer">
+                            <button onClick={() =>handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
     );
 }
 
